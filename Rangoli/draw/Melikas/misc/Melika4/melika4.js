c3ba0b16ee0580f0d6df802cff75b55a3f11e66c
GIT-Saiee <sreesaiee@gmail.com>
2025-02-15 16:03:28 -0800

    Added some rangoli designs and organized folders

---
+++ Rangoli/draw/Melikas/misc/Melika4/melika4.js
@@ -0,0 +1,27 @@
+import {PageFlip} from "https://cdn.jsdelivr.net/npm/page-flip@2.0.7/dist/js/page-flip.module.min.js";
+// import {PageFlip} from "./page-flip.module.js";
+document.addEventListener('DOMContentLoaded', function() {
+
+    const pageFlip2 = new PageFlip(
+        document.getElementById("demoBookExample2"),
+        {
+            width: 300, // base page width
+            height: 300, // base page height
+
+            size: "stretch",
+            // set threshold values:
+            minWidth: 315,
+            maxWidth: 500,
+            minHeight: 420,
+            maxHeight: 1350,
+
+            maxShadowOpacity: 0.5, // Half shadow intensity
+            showCover: true,
+            mobileScrollSupport: false // disable content scrolling on mobile devices
+        }
+    );
+   
+     
+ 
+ pageFlip2.loadFromImages(['../images/melika4/8.jpg','../images/melika4/1.jpg', '../images/melika4/2.jpg','../images/melika4/3.jpg','../images/melika4/4.jpg', '../images/melika4/5.jpg','../images/melika4/6.jpg','../images/melika4/7.jpg', '../images/melika4/8.jpg']);
+    }); 
---
+++ Rangoli/draw/Melikas/misc/Melika5/melika5.js
@@ -0,0 +1,27 @@
+import {PageFlip} from "https://cdn.jsdelivr.net/npm/page-flip@2.0.7/dist/js/page-flip.module.min.js";
+// import {PageFlip} from "./page-flip.module.js";
+document.addEventListener('DOMContentLoaded', function() {
+
+    const pageFlip2 = new PageFlip(
+        document.getElementById("demoBookExample2"),
+        {
+            width: 300, // base page width
+            height: 300, // base page height
+
+            size: "stretch",
+            // set threshold values:
+            minWidth: 315,
+            maxWidth: 500,
+            minHeight: 420,
+            maxHeight: 1350,
+
+            maxShadowOpacity: 0.5, // Half shadow intensity
+            showCover: true,
+            mobileScrollSupport: false // disable content scrolling on mobile devices
+        }
+    );
+   
+     
+ 
+ pageFlip2.loadFromImages(['../images/melika5/12.jpg','../images/melika5/1.jpg', '../images/melika5/2.jpg','../images/melika5/3.jpg','../images/melika5/4.jpg', '../images/melika5/5.jpg','../images/melika5/7.jpg', '../images/melika5/8.jpg','../images/melika5/9.jpg', '../images/melika5/10.jpg','../images/melika5/11.jpg', '../images/melika5s/12.jpg']);
+    }); 
