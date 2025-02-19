76805e99a44bbc3d34366e4c9ce0f0dbbba10c6b
GIT-Saiee <sreesaiee@gmail.com>
2025-02-18 19:29:08 -0800

    Added Melika muggu

--- Rangoli/draw/Melikas/misc/Melika1/9squares.js
+++ Rangoli/draw/Melikas/misc/Melika1/9squares.js
@@ -23,5 +23,5 @@ document.addEventListener('DOMContentLoaded', function() {
    
      
  
- pageFlip2.loadFromImages(['../images/melika1/23.png','../images/melika1/1.png', '../images/melika1/2.png','../images/melika1/3.png','../images/melika1/4.png', '../images/melika1/5.png','../images/melika1/6.png','../images/melika1/7.png', '../images/melika1/8.png','../images/melika1/9.png','../images/melika1/10.png', '../images/melika1/11.png','../images/melika1/12.png','../images/melika1/13.png', '../images/melika1/14.png','../images/melika1/15.png','../images/melika1/16.png','../images/melika1/17.png','../images/melika1/18.png', '../images/melika1/19.png','../images/melika1/20.png','../images/melika1/21.png', '../images/melika1/22.png','../images/melika1/23.png']);
+ pageFlip2.loadFromImages(['../images/melika1/10.jpg','../images/melika1/1.jpg', '../images/melika1/2.jpg','../images/melika1/3.jpg','../images/melika1/4.jpg', '../images/melika1/5.jpg','../images/melika1/6.jpg','../images/melika1/7.jpg', '../images/melika1/8.jpg','../images/melika1/9.jpg','../images/melika1/10.jpg']);
     }); 
