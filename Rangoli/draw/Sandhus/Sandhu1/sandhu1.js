27127f6279f469b6d28e57b125ad238dffd37bc5
GIT-Saiee <sreesaiee@gmail.com>
2025-02-20 15:06:40 -0800

    added oil painting

--- Rangoli/draw/Sandhus/Sandhu1/sandhu1.js
+++ Rangoli/draw/Sandhus/Sandhu1/sandhu1.js
@@ -1,4 +1,4 @@
-import {PageFlip} from "./page-flip.module.js";
+import {PageFlip} from "../page-flip.module.js";
 document.addEventListener('DOMContentLoaded', function() {
 
 
@@ -21,5 +21,6 @@ document.addEventListener('DOMContentLoaded', function() {
      }
  );
  pageFlip1.loadFromHTML(document.querySelectorAll('.my-page'));
- pageFlip1.loadFromImages(['images/18.jpg','images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg','images/5.jpg','images/6.jpg','images/7.jpg','images/8.jpg','images/9.jpg','images/11.jpg','images/12.jpg','images/13.jpg','images/14.jpg','images/15.jpg','images/16.jpg','images/17.jpg','images/18.jpg']);
+ pageFlip1.loadFromImages(['images/17.jpg','images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg','images/5.jpg','images/6.jpg','images/7.jpg','images/8.jpg','images/9.jpg','images/11.jpg','images/12.jpg','images/13.jpg','images/14.jpg','images/15.jpg','images/16.jpg','images/17.jpg']);
+
 });
