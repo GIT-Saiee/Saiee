1d4d15ad1a4071edc2f27f969b04206552aa241c
GIT-Saiee <sreesaiee@gmail.com>
2025-02-27 20:55:37 -0800

    added comments section in sandhus

--- Rangoli/draw/Sandhus/Sandhu3/sandhu3.js
+++ Rangoli/draw/Sandhus/Sandhu3/sandhu3.js
@@ -20,6 +20,6 @@ document.addEventListener('DOMContentLoaded', function() {
          mobileScrollSupport: false // disable content scrolling on mobile devices
      }
  );
- pageFlip1.loadFromHTML(document.querySelectorAll('.my-page'));
+ 
  pageFlip1.loadFromImages(['images/15.jpg','images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg','images/5.jpg','images/6.jpg','images/7.jpg','images/8.jpg','images/9.jpg','images/11.jpg','images/12.jpg','images/13.jpg','images/14.jpg','images/15.jpg']);
 });
