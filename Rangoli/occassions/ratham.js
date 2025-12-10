5fbe23d05b8a6ac8a92465884e49332e738c2236
GIT-Saiee <sreesaiee@gmail.com>
2025-12-10 12:06:43 -0800

    added new image file

--- Rangoli/occassions/ratham.js
+++ Rangoli/occassions/ratham.js
@@ -20,6 +20,6 @@ document.addEventListener('DOMContentLoaded', function() {
      }
  );
  
- pageFlip1.loadFromImages(['images/ratham/ratha17.png','images/ratham/dot.png', 'images/ratham/dot1.png','images/ratham/ratha1.png','images/ratham/ratha2.png','images/ratham/ratha3.png','images/ratham/ratha4.png','images/ratham/ratha5.png','images/ratham/ratha6.png','images/ratham/ratha7.png','images/ratham/ratha8.png','images/ratham/ratha9.png','images/ratham/ratha10.png','images/ratham/ratha11.png','images/ratham/ratha12.png','images/ratham/ratha13.png','images/ratham/ratha14.png','images/ratham/ratha15.png','images/ratham/ratha16.png','images/ratham/ratha17.png']);
+ pageFlip1.loadFromImages(['images/ratham/ratha18.png','images/ratham/dot.png', 'images/ratham/dot1.png','images/ratham/ratha1.png','images/ratham/ratha2.png','images/ratham/ratha3.png','images/ratham/ratha4.png','images/ratham/ratha5.png','images/ratham/ratha6.png','images/ratham/ratha7.png','images/ratham/ratha8.png','images/ratham/ratha9.png','images/ratham/ratha10.png','images/ratham/ratha11.png','images/ratham/ratha12.png','images/ratham/ratha13.png','images/ratham/ratha14.png','images/ratham/ratha15.png','images/ratham/ratha16.png','images/ratham/ratha17.png']);
 
 });
